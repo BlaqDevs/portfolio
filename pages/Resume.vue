7292ce289bf9ca72bb944a7e8be75d9f70295d0b
BlaqDevs <adebomor@gmail.com>
2021-10-22 02:20:15 +0100

    done with resume

--- pages/Resume.vue
+++ pages/Resume.vue
@@ -1,21 +1,70 @@
 <template>
-  <div class="mt-16">
-    <v-card class="" elevation="" max-width="">
-      <v-card-title primary-title>
-        title
-      </v-card-title>
-      <v-card-text>
-        <p class=""></p>
-        <div class="text--primary"></div>
-      </v-card-text>
-      <v-card-actions>
-        <v-btn text color=""> </v-btn>
-      </v-card-actions>
-    </v-card>
-  </div>
+  <v-app>
+    <nav>
+      <v-app-bar class="primary" dark dense app>
+        <v-action class="primary">
+          <v-btn icon small to="/" color="">
+            <v-icon color="">
+              mdi-chevron-left
+            </v-icon>
+          </v-btn>
+        </v-action>
+        <v-card-title primary-title>
+          Resume
+        </v-card-title>
+        <v-spacer />
+        <div>
+          <v-card-actions>
+            <a id="downloader" :href="resume">
+              <v-btn>
+                <span class="hidden-xs-only">Download</span>
+                <v-icon color="" right>
+                  mdi-download
+                </v-icon>
+              </v-btn>
+            </a>
+            <v-btn @click="$refs.pdf_viewer.print()">
+              <span class="hidden-xs-only">Print</span>
+              <v-icon color="" right>
+                mdi-printer
+              </v-icon>
+            </v-btn>
+          </v-card-actions>
+        </div>
+      </v-app-bar>
+    </nav>
+    <div id="resume-wrapper">
+      <pdf ref="pdf_viewer" :page="1" :src="resume" />
+      <v-divider />
+      <pdf ref="pdf_viewer" :page="2" :src="resume" />
+    </div>
+  </v-app>
 </template>
 <script>
+import pdf from 'vue-pdf'
 export default {
-  layout: 'resume'
+  components: {
+    pdf
+  },
+  layout: 'resume',
+  data () {
+    return {
+      resume: require('~/static/Omoyokan-Moses-Adenomor.pdf')
+    }
+  },
+  mounted () {
+    const dwnloader = document.getElementById('downloader')
+    dwnloader.download = 'overcomeremiator_resume'
+  }
 }
 </script>
+
+<style scoped>
+#resume-wrapper {
+  width: 100%;
+  height: calc(100vh - 47.8px);
+  background-color: #000;
+  color: #000;
+  overflow: auto;
+}
+</style>
